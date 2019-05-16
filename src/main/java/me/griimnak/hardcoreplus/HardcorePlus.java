31b8eb56307ab885ce9f94c837e13687e4c92dae
griimnak <griimnak@gmail.com>
2019-05-15 21:33:13 -0400

    Gameplay changes and improvements
    Killing the ender dragon now restores full max hp
    
    corrected a bug where creeper instakills you
    
    added blood effect, sound effect on death
    
    added permission:
    hardcoreplus.admin
    
    for admin new admin commands:
    disable, enable, setmax <player> <value>

--- src/main/java/me/griimnak/hardcoreplus/HardcorePlus.java
+++ src/main/java/me/griimnak/hardcoreplus/HardcorePlus.java
@@ -1,15 +1,14 @@
 package me.griimnak.hardcoreplus;
 
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EnderDragon;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.*;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -22,64 +21,105 @@ public final class HardcorePlus extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onPlayerDamage(EntityDamageEvent event) {
-        // Test if entity is a Player
-        if(event.getEntity() instanceof Player) {
-            Player player = (Player) event.getEntity();
-            // Calculate health difference
-            double result_hp = player.getHealth() - event.getDamage();
-            // Get current max health value
-            double max_hp = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
-            // If calculated health difference is less then or equal to 0
-            if (result_hp <= 0.0D) {
-                double result_max_hp = max_hp - 4.0D;
-                if(result_max_hp > 0.0D) {
-                    // Simulate full health
-                    player.setHealth(max_hp);
-                    // Simulate full food level
-                    player.setSaturation(5);
-                    player.setFoodLevel(20);
-                    player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(max_hp - 4.0D);
-                    player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "You have permanently lost health.");
+    public void playerDamageReceive(EntityDamageEvent e) {
+        if(e.getEntity() instanceof Player) {
+            Player damaged = (Player) e.getEntity();
+            damaged.getWorld().playEffect(damaged.getLocation(), Effect.STEP_SOUND, Material.REDSTONE_BLOCK);
+            double max_hp = damaged.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
+            if((damaged.getHealth()-e.getDamage()) <= 0.0D) {
+                if(max_hp - 4.0D > 0.0D) {
+                    e.setCancelled(true);
+                    damaged.setHealth(max_hp);
+                    damaged.setSaturation(5);
+                    damaged.setFoodLevel(20);
+                    damaged.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(max_hp - 4.0D);
+                    damaged.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "You have permanently lost health.");
                     // respawn
-                    if(player.getBedSpawnLocation() != null) {
-                        player.teleport(player.getBedSpawnLocation());
-                        player.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "You awake from your dream in confusion.");
+                    if(damaged.getBedSpawnLocation() != null) {
+                        damaged.teleport(damaged.getBedSpawnLocation());
+                        damaged.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "You awake from your dream in confusion.");
                     } else {
-                        player.teleport(getServer().getWorld("world").getSpawnLocation());
-                        player.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "With no place of rest set, you awake from your dream, in familiar land.");
+                        damaged.teleport(damaged.getWorld().getSpawnLocation());
+                        damaged.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "With no place of rest set, you awake from your dream, in familiar land.");
                     }
                     // trippy effect
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 220,1));
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 600,1));
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 100,0));
-                    return;
-                } else {
-                    // Die
-                    return;
+                    damaged.getWorld().playEffect(damaged.getLocation(), Effect.ZOMBIE_CONVERTED_VILLAGER, 0);
+                    damaged.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 220,1));
+                    damaged.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 600,1));
+                    damaged.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 100,0));
+
                 }
             }
-        } else {
-            return;
         }
     }
 
     @EventHandler
     public void Death(PlayerDeathEvent event) {
         Player player = event.getEntity();
+        event.setDeathMessage(ChatColor.RED + "" +ChatColor.BOLD + player.getDisplayName() + " is permanently dead!");
+        player.sendMessage(ChatColor.DARK_GRAY + "" + ChatColor.ITALIC + "You are dead forever.");
+        player.setGameMode(GameMode.SPECTATOR);
+    }
 
-           event.setDeathMessage(ChatColor.RED + "" +ChatColor.BOLD + player.getDisplayName() + " is permanently dead!");
-           player.sendMessage(ChatColor.DARK_GRAY + "" + ChatColor.ITALIC + "You are dead forever.");
-           player.setGameMode(GameMode.SPECTATOR);
-
+    @EventHandler
+    public void onEnderDragonKill(EntityDeathEvent event) {
+        // If Entity is Dragon
+        if(event.getEntity() instanceof EnderDragon) {
+            // For each player in the server
+            for(Player player: getServer().getOnlinePlayers()) {
+                // If player is in the end, reward full HP
+                if(player.getWorld().getEnvironment().equals(World.Environment.THE_END)) {
+                    player.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "The dream is over. Your mind clears, the portal opens.");
+                    // restore max health
+                    player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20.0D);
+                    player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Your max health has been restored!");
+                }
+            }
+        }
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(command.getName().equals("hardcoreplus")) {
-            sender.sendMessage(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Hardcore+ "+ getDescription().getVersion() +" by griimnak");
-            sender.sendMessage(ChatColor.GOLD + "Enhances the vanilla Minecraft hardcore experience by degrading players' max health on death.");
-            sender.sendMessage(ChatColor.GOLD + "Once you've ran out of hearts, you die permanently, the dream is over.");
+        if(command.getName().equalsIgnoreCase("hardcoreplus")) {
+            if(sender instanceof Player) {
+                if(args.length < 1) {
+                    sender.sendMessage(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Hardcore+ "+ getDescription().getVersion() +" by griimnak");
+                    sender.sendMessage(ChatColor.GOLD + "Enhances the vanilla Minecraft hardcore experience by degrading players' max health on death.");
+                    sender.sendMessage(ChatColor.GOLD + "Once you've ran out of hearts, you die permanently, the dream is over.");
+                    return true;
+                } else {
+                    if(!sender.hasPermission("hardcoreplus.admin")){
+                        sender.sendMessage(""+ ChatColor.RED+"You don't have the permission to do this!!!");
+                        return true;
+                    }
+                    if(args[0].equalsIgnoreCase("setmax")) {
+                        if(args.length < 3) {
+                            sender.sendMessage("usage: /hardcoreplus setmax <player> <max hp (20.0D default)>");
+                            return true;
+                        }
+                        if(getServer().getPlayer(args[1]) != null) {
+                            double max_hp = 20.0D;
+                            try {
+                                max_hp = Double.parseDouble(args[2]);
+                            } catch(NumberFormatException e) {
+                                sender.sendMessage("Invalid max HP value. Must be a double decimal like 20.0D");
+                            }
+                            if(max_hp < 0.0D) { sender.sendMessage("Max hp value should be 0.0D and above"); }
+                            Player player = getServer().getPlayer(args[1]);
+                            player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20.0D);
+                            player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Your max hp has been updated.");
+                            sender.sendMessage(""+args[1]+"'s max hp updated successfully.");
+                        } else {
+                            sender.sendMessage("User '"+args[1]+"' not found.");
+                        }
+
+                    } else if(args[0].equalsIgnoreCase("enable")) {
+                        sender.sendMessage("Not yet implemented");
+                    } else if(args[0].equalsIgnoreCase("disable")) {
+                        sender.sendMessage("Not yet implemented");
+                    }
+                }
+            }
         }
         return false;
     }
